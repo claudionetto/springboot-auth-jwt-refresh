f573b26e9819ce3826a6d0a2403863766d010ff6
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-04-22 16:40:29 -0300

    feat: initialize oauth2-jwt project

---
+++ src/main/java/com/claudionetto/jwtouath2/JwtOuath2Application.java
@@ -0,0 +1,13 @@
+package com.claudionetto.jwtouath2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOuath2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOuath2Application.class, args);
+	}
+
+}
